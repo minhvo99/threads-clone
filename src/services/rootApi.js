2146dfce704c6a04226a536a304af5a9e6bfa06d
minhvo99 <pikachu43dn@gmail.com>
2025-05-26 21:48:27 +0700

    fix issue redirec to home page when accessToken is null

--- src/services/rootApi.js
+++ src/services/rootApi.js
@@ -16,7 +16,7 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
     let result = await baseQuery(args, api, extraOptions);
     console.log('baseQueryWithForceLogout', { result });
 
-    if (result?.error?.status === 401) {
+    if (result?.error?.status === 401 || result?.error?.status === 'FETCH_ERROR') {
         if (result?.error?.data?.message === 'Token has expired.') {
             const refreshToken = api.getState().auth.refreshToken;
 
